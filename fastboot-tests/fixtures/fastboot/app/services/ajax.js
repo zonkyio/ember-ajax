db04c9818e670fc1cb5a0596013ad8f8082e272b
Taras Mankovski <tarasm@gmail.com>
2016-11-18 12:05:19 -0500

    Wrote first test

---
+++ fastboot-tests/fixtures/fastboot/app/services/ajax.js
@@ -0,0 +1,5 @@
+import EmberAjax from 'ember-ajax/services/ajax';
+
+export default EmberAjax.extend({
+  host: 'http://example.com'
+});
--- fastboot-tests/index-test.js
+++ fastboot-tests/index-test.js
@@ -1,7 +1,12 @@
 var expect = require('chai').expect;
+var nock = require('nock');
 
 describe('index', function() {
-
+  beforeEach(function() {
+    nock('http://example.com')
+      .get('/message')
+      .reply(200, 'hello world');
+  });
   it('renders', function() {
     return this.visit('/')
       .then(function(res) {
@@ -11,6 +16,7 @@ describe('index', function() {
         // add your real tests here
         expect($('body').length).to.equal(1);
         expect($('h1').text().trim()).to.equal('ember-fastboot-addon-tests');
+        expect($('.message').text()).to.equal('hello world');
       });
   });
 
